1cf84d3dda34774042e37299ae122bd24dd04995
Tyler Smith <tylertms@icloud.com>
2026-08-29 03:47:40 -0400

    feat(motor): recover ADC and PWM data paths

---
+++ source/common/motor/io.h
@@ -0,0 +1,22 @@
+#ifndef OPENTEC_MOTOR_IO_H
+#define OPENTEC_MOTOR_IO_H
+
+#include <fsl_device_registers.h>
+#include <gmclib.h>
+#include <stdbool.h>
+
+typedef struct {
+    frac16_t phase_a;
+    frac16_t phase_b;
+} MotorCurrentOffsets;
+
+typedef struct {
+    GMCLIB_3COOR_T_F16 phase_current;
+    frac16_t dc_bus_voltage;
+} MotorAdcSample;
+
+bool motor_adc_read(ADC_Type *adc0, ADC_Type *adc1, const MotorCurrentOffsets *offsets,
+                    MotorAdcSample *sample);
+void motor_pwm_write(FTM_Type *ftm, GMCLIB_3COOR_T_F16 *duty);
+
+#endif
